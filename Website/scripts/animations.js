b55e09a68729092b81435f1f5873fc069c0c219d
Soroban1729 <ruben.e.sanchez.a@hotmail.com>
2019-02-05 01:09:44 -0500

    New Structure set up

--- Website/scripts/animations.js
+++ Website/scripts/animations.js
@@ -2,13 +2,22 @@ $(document).ready(function() {
     $("#topBtn").click(function(){
 		$('html,body').animate({ scrollTop: 0 }, 'slow');
 		return false;
-	})
+	});
+
+    $(".aboutMeBtn").click(function (){scrollToElem("#aboutMe");});
+    $(".portfolioBtn").click(function (){scrollToElem("#portfolio");});
+    $(".contactBtn").click(function (){scrollToElem("#contact");});
+    $(".homeBtn").click(function(){
+		$('html,body').animate({ scrollTop: 0 }, 'slow');
+		return false;
+	});
+
 });
 
-function toTopAnimation(){
 
-	alert("animating...");
-	$('html,body').animate({ scrollTop: 0 }, 'slow');
-	return false;
+function scrollToElem(elemId){
+
+	$('html, body').animate({scrollTop: 
+		($(elemId).offset().top)}, 'slow');
 
 }
